097571830963b4ba02fe0192b50030ef60ef7f55
CataldoCocuzza <ccocuzza00@gmail.com>
2024-06-10 23:40:02 -0400

    pushing update

---
+++ PyBank/main.py
@@ -0,0 +1,69 @@
+import csv
+import os
+
+# Files to load and output
+load_file = os.path.join("/Users/ccocu/Desktop/Homework/python-challenge/PyBank/Resources", "budget_data.csv")
+output_file = os.path.join("/Users/ccocu/Desktop/Homework/python-challenge/PyBank/analysis", "results.txt")
+
+# Track various financial parameters
+total_months = 0
+month_of_change = []
+net_change_list = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999999999999999]
+total_net = 0
+
+# Read the csv
+with open(load_file) as financial_data:
+    reader = csv.reader(financial_data)
+
+    
+    header = next(reader)
+
+    # Extract first row
+    first_row = next(reader)
+    total_months += 1
+    total_net += int(first_row[1])
+    prev_net = int(first_row[1])
+
+    for row in reader:
+
+        # total
+        total_months += 1
+        total_net += int(row[1])
+
+        # net change
+        net_change = int(row[1]) - prev_net
+        prev_net = int(row[1])
+        net_change_list += [net_change]
+        month_of_change += [row[0]]
+
+        # Calculate the greatest increase
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
+
+        # Calculate the greatest decrease
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
+
+# Calculate the Average Net Change
+net_monthly_avg = sum(net_change_list) / len(net_change_list)
+
+# Generate Output Summary
+output = (
+    f"Financial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${total_net}\n"
+    f"Average Change: ${net_monthly_avg:.2f}\n"
+    f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+
+# Print the output (to terminal)
+print(output)
+
+# Export the results to text file
+with open(output_file, "w") as txt_file:
+    txt_file.write(output)
